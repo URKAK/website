b24ea6597e4cc83276efc6f5e715f49db33b73f9
URKAK <123753749+URKAK@users.noreply.github.com>
2024-08-25 13:21:23 +0300

    Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,12 @@
 
 const items = [
-        { icon: 'fab fa-instagram', link: 'https://instagram.com/umutxyp' },
-        { icon: 'fab fa-telegram', link: 'https://t.me/umutland' },
-    { icon: 'fab fa-youtube', link: 'https://youtube.com/UmutBayraktarYT' },
-    { icon: 'fab fa-facebook', link: 'https://facebook.com/umutxyp' },
-    { icon: 'fab fa-github', link: 'https://github.com/umutxyp' },
+    { icon: 'fab fa-youtube', link: 'https://youtube.com/@TKT-TAF' },
   ]
 
 const navItems = [
-    { icon: 'fal fa-home', active: 'fa fa-home', label: 'Home', href: '/' },
-    { icon: 'fal fa-compass', active: 'fa fa-compass', label: 'Projects', href: '/projects' },
-    { icon: 'fal fa-phone', active: 'fa fa-phone', label: 'Contact', href: '/contact' },
-    { icon: 'fal fa-rss', active: 'fa fa-rss', label: 'Blogs', href: 'https://blog.codeshare.me' }
+    { icon: 'fal fa-home', active: 'fa fa-home', label: 'Ana Sayfa', href: '/' },
+    { icon: 'fal fa-compass', active: 'fa fa-compass', label: 'Hakkımızda', href: '/projects' },
+    { icon: 'fal fa-phone', active: 'fa fa-phone', label: 'İletişim', href: '/contact' },
 ]
 
 import Link from 'next/link';
